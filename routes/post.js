277e53fb0567f9372692a175d5531600cd074c11
“Ketan <tayalket@gmail.com>
2020-03-25 16:04:56 +0530

    routes/post.js file added

---
+++ routes/post.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const verify = require('./verifyToken');
+router.get('/',verify,  (req,res)=>{
+    res.json({
+        posts:{
+            title:'my first post',
+            description : 'random data you should not access'
+        }
+    });
+});
+
+module.exports = router;
+
+
+
